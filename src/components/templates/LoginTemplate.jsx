58ffa5bd7c55bb6bbe440e8de24aab0714aa4318
Exequiel Guinazu <egc.criss@gmail.com>
2025-04-06 21:20:05 -0300

    creamos las primeras routes e instalamos las dependencias necesarias

---
+++ src/components/templates/LoginTemplate.jsx
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+export function LoginTemplate() {
+  return (
+    <Container>
+      <h1>LoginTemplate</h1>
+    </Container>
+  );
+}
+const Container = styled.div``;
---
+++ src/pages/Login.jsx
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+export function Login() {
+  return (
+    <Container>
+      <h1>Login</h1>
+    </Container>
+  );
+}
+const Container = styled.div``;
