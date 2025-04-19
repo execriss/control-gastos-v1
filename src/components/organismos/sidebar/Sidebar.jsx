dfa7eaad2a06c343ff321a117f1f296914f78212
Exequiel Guinazu <egc.criss@gmail.com>
2025-04-19 01:28:02 -0300

    logo sidebar

--- src/components/organismos/sidebar/Sidebar.jsx
+++ src/components/organismos/sidebar/Sidebar.jsx
@@ -5,7 +5,7 @@ import { NavLink } from "react-router-dom";
 export function Sidebar({ state, setState }) {
   return (
     <Main>
-      <Container>
+      <Container isOpen={state}>
         <div className="LogoContent">
           <div className="ImgContent">
             <img src={v.logo} alt="logo" />
@@ -32,6 +32,33 @@ const Container = styled.div`
   padding-top: 20px;
   z-index: 100;
   height: 100%;
+
+  .LogoContent {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    padding-bottom: 60px;
+
+    .ImgContent {
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      width: 30px;
+      cursor: pointer;
+      transition: 0.3s ease;
+      transform: ${({ isOpen }) => (isOpen ? `scale(0.7)` : `scale(1.5)`)}
+        rotate(${({ theme }) => theme.logorotate});
+
+      img {
+        width: 100%;
+        animation: flotar 1.7s ease-in-out infinite alternate;
+      }
+    }
+
+    h2 {
+      display: ${({ isOpen }) => (isOpen ? `block` : `none`)};
+    }
+  }
 `;
 
 const Main = styled.div``;
